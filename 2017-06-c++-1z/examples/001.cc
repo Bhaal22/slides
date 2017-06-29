462fdacf17c8b7b41ab55396c400b131166cd186
bhaal22 <muller.john@gmail.com>
2017-06-29 21:07:15 +0200

    basic core features

--- 2017-06-c++-1z/examples/001.cc
+++ 2017-06-c++-1z/examples/001.cc
@@ -12,6 +12,19 @@ struct A
   template<typename T>
   T f(T t)
   { return t + 2; }
+
+  virtual int virtual_f(int x)
+  { return x; }
+};
+
+struct B: public A
+{
+  B(std::initializer_list<int> integers)
+    : A(integers)
+  { }
+
+  virtual int virtual_f(double d) override
+  { return 0; }
 };
 
 template<typename T, typename U>
@@ -34,5 +47,10 @@ int main(int argc, char **argv)
   auto lambda = [](int x, int y) -> int { return x + y; };
 
   std::cout << lambda(2, 3) << std::endl;
+
+
+  B b({0});
+
+  std::cout << b.virtual_f((int)2) << std::endl;
   return 0;
 }
